a498e969da7f09d039f885b5082bc9e9fffc10e1
Janis Altherr <janisaltherr@gmail.com>
2018-12-25 20:34:47 +0000

    [fix] proper db connection URL

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
-
+require('dotenv').load();
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
 // Database
 // Connect to DB
+const { DB_USERNAME, DB_PASSWORD, DB_SERVER, DB_PORT, DB_NAME } = process.env;
 mongoose.connect(
-  'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>',
+  `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_SERVER}:${DB_PORT}/${DB_NAME}`,
   { useNewUrlParser: true })
   .catch(
     (err) => {
