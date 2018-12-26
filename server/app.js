dcf116afcfe8dbcd7aa2880a7aac087ec00f7130
Janis Altherr <jaltherr@gmail.com>
2018-12-26 15:03:40 +0000

    [change] websocket only server

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ require('dotenv').load();
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
+
 // Database
 // Connect to DB
 const { DB_USERNAME, DB_PASSWORD, DB_SERVER, DB_PORT, DB_NAME, DB_AUTHDB } = process.env;
@@ -13,3 +14,16 @@ mongoose.connect(
       console.log(`connection to the database failed with error: ${err}`);
     },
   );
+
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+  console.log('connection')
+  ws.send('something');
+});
+console.log('waiting');
