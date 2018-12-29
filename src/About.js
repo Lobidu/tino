76dcd47796efb6543059aba0fcf3f36ec6550afc
Janis Altherr <janisaltherr@gmail.com>
2018-12-29 22:46:52 +0000

    [update] readability improvements

---
+++ src/About.js
@@ -0,0 +1,18 @@
+export default {
+  name: 'About',
+  render() {
+    return `<div id="about" class="page center-elements">
+    <div class="text-center width-50">
+      <h2>I am a developer.</h2>
+      <p>
+          My name is Janis, I am a full stack developer currently 
+          living in Cork. I am passionate about the open web and 
+          determined to create beautiful interactive websites and 
+          web applications.
+      </p>
+      <p class="text-primary">
+          I'm ready to leave an impression.
+      </p>
+    </div>`
+  },
+};
