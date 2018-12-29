249eda934b652fc9900e335f4211d237cedd9c93
Janis Altherr <janisaltherr@gmail.com>
2018-12-29 22:46:30 +0000

    [update] improved scroll trigger

---
+++ src/Transition.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'Transition',
+  data: {
+    visitors: 49,
+  },
+  render() {
+   return `<div id="transition" class="fade center-elements">` +
+     `<div class="text-center width-50"><p>` +
+     ` By moving your cursor, you have left an impression on this website. ` +
+     ` To this day, ${this.visitors} users have left their individual mark.` +
+     `</p></div>` +
+     `</div>`
+  },
+  handleScroll() {
+    if(this.node.getBoundingClientRect().bottom < window.innerHeight){
+      this.node.classList.add("visible");
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', () => { this.handleScroll() });
+  }
+};
