cffa70d569bfa9f9d59c58bf64920504f5586737
Janis Altherr <janisaltherr@gmail.com>
2018-12-29 22:55:05 +0000

    [update] project restructuring

--- frontend/js/main.js
+++
@@ -1,7 +0,0 @@
-import Engine from './engine.js';
-import App from '../src/App.js';
-
-const rootElement = document.getElementById('App');
-const Root = new Engine(App, rootElement  );
-Root.mount();
-
