fde847a47c98040074008c04b7848bef12b319be
Janis Altherr <janisaltherr@gmail.com>
2019-01-01 22:28:04 +0000

    [update] fine-tuning magic canvas

--- src/MagicCanvas.js
+++ src/MagicCanvas.js
@@ -5,64 +5,93 @@ export default {
   },
   data: {
     gridSize: 100,
-    maximumIntensity: 5
+    maxIntensity: 1,
+    mousetrace: [],
+  },
+  initializeCanvasMeasures(){
+    this.canvas = this.node.getElementsByTagName('canvas')[0];
+    this.cv = {};
+    this.cv.boundingRect = this.canvas.getBoundingClientRect();
+    this.cv.largerDimension = Math.max(this.cv.boundingRect.height, this.cv.boundingRect.width);
+    // Calculate the overflowing part in pixels, divide by 2, half for each end
+    const pxOverflowX = (this.cv.largerDimension - this.cv.boundingRect.width) / 2;
+    const pxOverflowY = (this.cv.largerDimension - this.cv.boundingRect.height) / 2;
+    // Convert this to the amount of grid squares
+    this.cv.overflowX = Math.floor(pxOverflowX * this.gridSize / this.cv.largerDimension);
+    this.cv.overflowY  = Math.floor(pxOverflowY * this.gridSize / this.cv.largerDimension);
+    // Calculate the cv size in numbers of squares
+    this.cv.squareCountX = this.gridSize - this.cv.overflowX;
+    this.cv.squareCountY = this.gridSize - this.cv.overflowY;
+    return true;
   },
   getColor(intensity){
-    const hex = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
     const relativeIntensity = intensity/this.maxIntensity;
-    const intensityHexValue = Math.round(relativeIntensity * hex.length);
-    const b = hex[intensityHexValue] || 'f';
-    return `#0000${b}${b}`;
+    return `rgba(0, 0, 255, ${relativeIntensity})`;
   },
   drawSquare(x, y, intensity){
     if(intensity <= 0) return;
-    const squareSize = this.biggerDimension / this.gridSize;
-    const posX = Math.floor((x - this.gridOverflowX)/this.gridSize * this.canvasRect.width);
-    const posY = Math.floor((y - this.gridOverflowY)/this.gridSize * this.canvasRect.height);
-    const ctx = this.canvas.getContext("2d");
+    const squareSize = this.cv.largerDimension / this.gridSize;
+    const posX = Math.floor((x - this.cv.overflowX)/this.gridSize * this.cv.largerDimension);
+    const posY = Math.floor((y - this.cv.overflowY)/this.gridSize * this.cv.largerDimension);
+    const canvas = this.node.getElementsByTagName('canvas')[0];
+    const ctx = canvas.getContext("2d");
     ctx.fillStyle = this.getColor(intensity);
     ctx.fillRect(posX,posY,squareSize,squareSize);
+    return {ctx, posX, posY, squareSize};
   },
   paintCanvas(){
-    this.canvasRect = this.canvas.getBoundingClientRect();
-    this.biggerDimension = Math.max(this.canvasRect.height, this.canvasRect.width);
-    // Calculate the overflowing part in pixels, divide by 2, half for each end
-    const overflowX = (this.biggerDimension - this.canvasRect.width) / 2;
-    const overflowY = (this.biggerDimension - this.canvasRect.height) / 2;
-    // Convert this to the amount of grid squares
-    this.gridOverflowX = Math.floor(overflowX * this.gridSize / this.biggerDimension);
-    this.gridOverflowY = Math.floor(overflowY * this.gridSize / this.biggerDimension);
-    // Calculate the canvas size in numbers of squares
-    const canvasSquareCountX = this.gridSize - this.gridOverflowX;
-    const canvasSquareCountY = this.gridSize - this.gridOverflowY;
-    for(let x=this.gridOverflowX; x<canvasSquareCountX; x++){
-      for(let y=this.gridOverflowY; y<canvasSquareCountY; y++){
+    for(let x=this.cv.overflowX; x<this.cv.squareCountX; x++){
+      for(let y=this.cv.overflowY; y<this.cv.squareCountY; y++){
         const intensity = this.grid[x][y];
-        // convert the grid values back into actual pixel coordinates
         this.drawSquare(x, y, intensity)
       }
     }
   },
   getEmptyGrid(){
     // Initializes a two-dimensional array with all values set to 0.
-    const initialValue = 5;
+    const initialValue = 0;
     const col = Array(this.gridSize).fill(initialValue);
     return Array(this.gridSize).fill(col);
   },
   getMousePositionOnGrid(mousePos){
     const [mouseX, mouseY] = mousePos;
-    const rect = this.canvas.getBoundingClientRect();
-    const x = Math.floor((mouseX - rect.left) * this.gridSize / this.biggerDimension);
-    const y = Math.floor((mouseY - rect.top) * this.gridSize / this.biggerDimension);
+    const rect = this.cv.boundingRect;
+    const x = Math.floor((mouseX - rect.left) * this.gridSize / this.cv.largerDimension) + this.cv.overflowX;
+    const y = Math.floor((mouseY - rect.top) * this.gridSize / this.cv.largerDimension) + this.cv.overflowY;
     return [x,y];
   },
   handleMouseMove(pos) {
     const [x,y] = this.getMousePositionOnGrid([pos.clientX,pos.clientY]);
-    //this.ws.send(JSON.stringify([x,y]));
+    this.ws.send(JSON.stringify([x,y]));
     this.grid[x][y]++;
-    this.paintCanvas();
-    //this.drawSquare(x, y, this.maxIntensity * 0.3);
-    //this.drawDiminishingSquare(x, y)
+    //this.paintCanvas();
+    //this.drawSquare(x, y, this.grid[x][y]+0.2*this.maxIntensity);
+    const intensity = this.grid[x][y]+0.5*this.maxIntensity;
+    const square = this.drawSquare(x, y, intensity);
+    this.fadeSquare(square, intensity, this.grid[x][y])
+
+  },
+  fadeSquare(square, fadeFromIntensity, fadeToIntensity){
+    if (fadeFromIntensity < fadeToIntensity) return;
+    const steps = Math.round(this.maxIntensity / 100);
+    setTimeout(
+      () => {
+        square.ctx.fillStyle = this.getColor(fadeFromIntensity-steps);
+        square.ctx.fillRect(square.posX, square.posY, square.squareSize, square.squareSize)
+      },
+      100
+    )
+  },
+  drawDiminishingSquare(x, y, intensity){
+    if (intensity < this.grid[x][y]) return;
+    const steps = Math.round(this.maxIntensity / 100);
+    const ctx = this.drawSquare(x, y, intensity);
+    setTimeout(
+      ()=>{
+        this.drawDiminishingSquare(x, y, intensity-steps);
+      },
+      100
+    )
   },
   establishSocket(){
     return new Promise(resolve => {
@@ -88,12 +117,14 @@ export default {
       this.paintCanvas();
     };
   },
-  mounted() {
-
-    this.canvas = this.node.getElementsByTagName('canvas')[0];
+  initialize(){
     this.grid = this.getEmptyGrid();
-    //this.initializeRemote();
-    this.paintCanvas();
-    //window.addEventListener("mousemove", (pos)=>(this.handleMouseMove(pos)), false);
+    this.initializeCanvasMeasures();
+    this.initializeRemote();
+  },
+  mounted() {
+    this.initialize();
+    window.addEventListener("mousemove", (pos)=>(this.handleMouseMove(pos)), false);
+    window.addEventListener("resize", ()=>(this.initialize()), false);
   },
 }
