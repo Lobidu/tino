8a6de9e25ffa6c55b8f25894a69a156f9f389102
Janis Altherr <janisaltherr@gmail.com>
2019-05-07 18:35:56 +0100

    update wording

---
+++ src/views/Footer.js
@@ -0,0 +1,11 @@
+export default {
+  name: 'Footer',
+  render: () => (
+    `<div id="footer" class="page center-elements">
+     <div class="text-center">
+     This page was proudly handcrafted using nothing but plain Javascript.
+     The graphic on the top is driven by a Node.js server.<br/>
+     <a href="https://github.com/Lobidu/tino">View it on Github</a>.
+     </div>
+     </div>`),
+};
